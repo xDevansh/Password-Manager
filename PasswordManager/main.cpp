687dc017c745f45157e3fcbaa17573287a9cb669
xDevansh <devjollani@Gmail.com>
2025-08-06 19:39:47 +0530

    added functionality of adding a password in plaintext, the text is appended at the end of the file, will focus on viewing stored passwords and applying basic level encryption such as xor encryption next

--- PasswordManager/main.cpp
+++ PasswordManager/main.cpp
@@ -1,5 +1,36 @@
 #include <iostream>  
+#include<string>
+#include<fstream>
+
 using namespace std;
+
+
+void addPassword() { //functionality for adding password
+    string website, username, password;
+
+    cout << "\nEnter Website Name: ";
+    cin >> website;
+
+    cout << "Enter Username or Email: ";
+    cin >> username;
+
+    cout << "Enter Password: ";
+    cin >> password;
+
+    // use ofstream to open file in append mode to add new entry at the end
+    ofstream file("passwords.txt", ios::app);  
+
+    if (file.is_open()) {
+ 
+        file << website << " " << username << " " << password << endl;
+        cout << "Password saved successfully!\n";
+    }
+    else {
+        cout << "Error: Could not open file for writing.\n";
+    }
+
+    file.close();  //closing the file
+}
 
 int main() {
     int choice;
@@ -14,7 +45,7 @@ int main() {
 
         switch (choice) {
         case 1:
-            cout << "Add Password functionality will go here.\n";
+            addPassword();
             break;
         case 2:
             cout << "View Passwords functionality will go here.\n";
